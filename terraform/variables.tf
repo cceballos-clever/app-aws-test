b1cbd48c22b031669cfe27bd4212ab0ee9e4613e
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-08 10:01:10 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,3 +2,11 @@ variable "region" {}
 variable "access_key" {}
 variable "secret_key" {}
 variable "ami_id" {}
+variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
+}
+variable "db_password" {
+  sensitive = true
+}
+variable "domain_name" {}
