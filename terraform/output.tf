7fdbe5c86eb424fd9340104c036aae93347a63c2
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-08 10:02:55 -0300

    Update output.tf

--- terraform/output.tf
+++ terraform/output.tf
@@ -1,3 +1,11 @@
 output "instance_public_ip" {
   value = aws_instance.web.public_ip
 }
+
+output "alb_dns" {
+  value = aws_lb.app_lb.dns_name
+}
+
+output "rds_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
