6d6d13e17f3b737b221f022230d7249bef687aee
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-08 10:58:10 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,35 +1,12 @@
-variable "vpc_id" {
-  description = "VPC donde se despliega todo"
-  type        = string
+variable "region" {}
+variable "access_key" {}
+variable "secret_key" {}
+variable "ami_id" {}
+variable "vpc_id" {}
+variable "subnet_ids" {
+  type = list(string)
 }
-
-variable "public_subnet_ids" {
-  description = "Lista de subnets públicas"
-  type        = list(string)
-}
-
-variable "private_subnet_ids" {
-  description = "Lista de subnets privadas"
-  type        = list(string)
-}
-
-variable "ami_id" {
-  description = "AMI ID para la instancia EC2"
-  type        = string
-}
-
-variable "db_username" {
-  description = "Usuario para la base de datos PostgreSQL"
-  type        = string
-}
-
 variable "db_password" {
-  description = "Password para la base de datos PostgreSQL"
-  type        = string
-  sensitive   = true
+  sensitive = true
 }
-
-variable "domain_name" {
-  description = "Dominio para Route53"
-  type        = string
-}
+variable "domain_name" {}
