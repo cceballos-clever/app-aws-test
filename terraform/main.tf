1a6c8ec736b18538c455c1b137f3cd5b6d423aa9
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-11 16:26:48 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-  region = var.region
-  access_key = var.access_key
-  secret_key = var.secret_key
-}
-
 # ========================
 # 1. VPC y Subnets públicas
 # ========================
