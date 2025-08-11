b09142517b74f5ef72255fa51226c7c1ce3ed222
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-11 11:21:14 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -11,12 +11,12 @@ variable "domain_name" {}
 
 variable "public_subnet_ids" {
   description = "List of public subnet IDs"
-  type        = list(string)
+  type        = string
 }
 
 variable "private_subnet_ids" {
   description = "List of private subnet IDs"
-  type        = list(string)
+  type        = string
 }
 
 variable "subnet_ids" {
