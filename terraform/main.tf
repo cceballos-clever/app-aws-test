c7403573db9f6250ffe432a0d0070b7ab57cb64f
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-08-11 12:25:48 -0300

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,7 @@ resource "aws_eip" "nat_eip" {
 # NAT Gateway en subnet pública
 resource "aws_nat_gateway" "nat" {
   allocation_id = aws_eip.nat_eip.id
-  subnet_id     = var.public_subnet_id
+  subnet_id     = var.public_subnet_id[0]
 
   tags = {
     Name = "main-nat-gateway"
@@ -101,7 +101,7 @@ resource "aws_lb" "app_lb" {
   internal           = false
   load_balancer_type = "application"
   security_groups    = [aws_security_group.alb_sg.id]
-  subnet            = var.public_subnet_id
+  subnets            = var.public_subnet_ids
 }
 
 # Target Group para ALB
@@ -137,7 +137,7 @@ resource "aws_lb_listener" "http_listener" {
 resource "aws_instance" "app_server" {
   ami           = var.ami_id
   instance_type = "t3.micro"
-  subnet_id     = var.private_subnet_id
+  subnet_id     = var.private_subnet_id[0]
   security_groups = [aws_security_group.alb_sg.id]
 
   tags = {
@@ -148,7 +148,7 @@ resource "aws_instance" "app_server" {
 # RDS PostgreSQL
 resource "aws_db_subnet_group" "rds_subnet_group" {
   name       = "rds-subnet-group"
-  subnet_ids = var.private_subnet_id
+  subnet_ids = var.private_subnet_ids
 }
 
 resource "aws_db_instance" "postgres" {
