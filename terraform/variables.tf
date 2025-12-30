7d0a0876bf6dfca69836d4c7c26791b310d3a892
CeballosCristian <132315741+cceballos-clever@users.noreply.github.com>
2025-12-30 10:17:03 -0300

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -16,27 +16,6 @@ variable "secret_key" {
   sensitive   = true
 }
 
-variable "ami_id" {
-  description = "AMI ID para la instancia EC2"
-  type        = string
-}
-
-variable "db_username" {
-  description = "Usuario para la base de datos"
-  type        = string
-}
-
-variable "db_password" {
-  description = "Contraseña para la base de datos"
-  type        = string
-  sensitive   = true
-}
-
-variable "key_name" {
-  description = "Nombre del key pair para EC2"
-  type        = string
-}
-
 variable "domain_name" {
   description = "Nombre de dominio para Route53"
   type        = string
